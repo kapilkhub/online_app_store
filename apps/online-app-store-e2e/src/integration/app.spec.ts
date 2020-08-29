fe074ffa1eb168d762a739ecc0021121e7346b9a
Kapil Khubchandani <kk.jecrc@gmail.com>
2020-08-29 12:10:46 +0400

    initial commit

---
+++ apps/online-app-store-e2e/src/integration/app.spec.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('online-app-store', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome to online-app-store!');
+  });
+});
